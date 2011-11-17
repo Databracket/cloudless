582b0d9b3c2df26c807f05604a8f5626dcc3fd5e
Amr Ali <amr@databracket.com>
2011-11-17 02:22:12 +0200

    Add wrappers around all 0MQ socket options to maintain a consistent interface across versions

--- cloudless/socket.cpp
+++ cloudless/socket.cpp
@@ -23,6 +23,13 @@
 #include <cloudless/poller.hpp>
 #include <cloudless/exceptions.hpp>
 
+#define _M_getsockopt(opt_, optval_) \
+    size_t optvallen = sizeof(optval_); \
+    getsockopt(opt_, (void*)&optval_, &optvallen);
+
+#define _M_setsockopt(opt_, optval_) \
+    setsockopt(opt_, (void*)&optval_, sizeof(optval_));
+
 namespace cloudless
 {
 
@@ -130,23 +137,376 @@ namespace cloudless
         return true;
     }
 
-    void
-    socket::set_send_timeout(int timeout_)
+    uint64_t
+    socket::affinity() const
     {
-        _M_sendtimeo = timeout_;
+        uint64_t affinity;
+
+        _M_getsockopt(ZMQ_AFFINITY, affinity);
+        return affinity;
+    }
+
+    std::string
+    socket::identity() const
+    {
+        // TODO
+
+        return std::string();
+    }
+
+    int
+    socket::rate() const
+    {
+        int64v rate;
+
+        _M_getsockopt(ZMQ_RATE, rate);
+        return rate;
+    }
+
+    int
+    socket::recovery_ivl() const
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int opt = ZMQ_RECOVERY_IVL;
+#elif ZMQ_VERSION_MAJOR == 2
+        int opt = ZMQ_RECOVERY_IVL_MSEC;
+#endif
+        int64v recovery_ivl;
+
+        _M_getsockopt(opt, recovery_ivl);
+        return recovery_ivl;
+    }
+
+    int
+    socket::send_buffer() const
+    {
+        uint64v buffer;
+
+        _M_getsockopt(ZMQ_SNDBUF, buffer);
+        return buffer;
+    }
+
+    int
+    socket::recv_buffer() const
+    {
+        uint64v buffer;
+
+        _M_getsockopt(ZMQ_RCVBUF, buffer);
+        return buffer;
+    }
+
+    bool
+    socket::recv_more() const
+    {
+        int64v more;
+
+        _M_getsockopt(ZMQ_RCVMORE, more);
+        return more;
+    }
+
+    socket_t
+    socket::fd() const
+    {
+        socket_t fd;
+
+        _M_getsockopt(ZMQ_FD, fd);
+        return fd;
+    }
+
+    bool
+    socket::can_send() const
+    {
+        return _M_get_events() & ZMQ_POLLOUT;
+    }
+
+    bool
+    socket::can_recv() const
+    {
+        return _M_get_events() & ZMQ_POLLIN;
+    }
+
+    int
+    socket::type() const
+    {
+        int type;
+
+        _M_getsockopt(ZMQ_TYPE, type);
+        return type;
+    }
+
+    int
+    socket::linger() const
+    {
+        int linger;
+
+        _M_getsockopt(ZMQ_LINGER, linger);
+        return linger;
+    }
+
+    int
+    socket::reconnect_ivl() const
+    {
+        int ivl;
+
+        _M_getsockopt(ZMQ_RECONNECT_IVL, ivl);
+        return ivl;
+    }
+
+    int
+    socket::reconnect_ivl_max() const
+    {
+        int ivl_max;
+
+        _M_getsockopt(ZMQ_RECONNECT_IVL_MAX, ivl_max);
+        return ivl_max;
+    }
+
+    int
+    socket::backlog() const
+    {
+        int backlog;
+
+        _M_getsockopt(ZMQ_BACKLOG, backlog);
+        return backlog;
+    }
+
+    int64_t
+    socket::max_msg_size() const
+    {
+        int64_t mms;
 
 #if ZMQ_VERSION_MAJOR == 3
-        setsockopt(ZMQ_SNDTIMEO, &_M_sendtimeo, sizeof(_M_sendtimeo));
+        _M_getsockopt(ZMQ_MAXMSGSIZE, mms);
+#elif ZMQ_VERSION_MAJOR == 2
+        mms = _M_max_msg_size;
+#endif
+
+        return mms;
+    }
+
+    int
+    socket::send_hwm() const
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int hwm_type = ZMQ_SNDHWM;
+#elif ZMQ_VERSION_MAJOR == 2
+        int hwm_type = ZMQ_HWM;
+#endif
+        uint64v hwm;
+
+        _M_getsockopt(hwm_type, hwm);
+        return hwm;
+    }
+
+    int
+    socket::recv_hwm() const
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int hwm_type = ZMQ_RCVHWM;
+#elif ZMQ_VERSION_MAJOR == 2
+        int hwm_type = ZMQ_HWM;
+#endif
+        uint64v hwm;
+
+        _M_getsockopt(hwm_type, hwm);
+        return hwm;
+    }
+
+    int
+    socket::multicast_hops() const
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int hops;
+
+        _M_getsockopt(ZMQ_MULTICAST_HOPS, hops);
+        return hops;
+#elif ZMQ_VERSION_MAJOR == 2
+        return -1;
+#endif
+    }
+
+    int
+    socket::send_timeout() const
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int timeo;
+
+        _M_getsockopt(ZMQ_SNDTIMEO, timeo);
+        return timeo;
+#elif ZMQ_VERSION_MAJOR == 2
+        return _M_sendtimeo;
+#endif
+    }
+
+    int
+    socket::recv_timeout() const
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int timeo;
+
+        _M_getsockopt(ZMQ_RCVTIMEO, timeo);
+        return timeo;
+#elif ZMQ_VERSION_MAJOR == 2
+        return _M_recvtimeo;
+#endif
+    }
+
+    bool
+    socket::ipv4_only() const
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int ipv4;
+
+        _M_getsockopt(ZMQ_IPV4ONLY, ipv4);
+        return ipv4;
+#elif ZMQ_VERSION_MAJOR == 2
+        return true;
 #endif
     }
 
     void
-    socket::set_recv_timeout(int timeout_)
+    socket::affinity(uint64_t affinity_)
+    {
+        _M_setsockopt(ZMQ_AFFINITY, affinity_);
+    }
+
+    void
+    socket::identity(const std::string& identity_)
+    {
+        // TODO
+    }
+
+    void
+    socket::rate(int rate_)
+    {
+        int64v rate = rate_;
+
+        _M_setsockopt(ZMQ_RATE, rate);
+    }
+
+    void
+    socket::recovery_ivl(int ivl_)
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int opt = ZMQ_RECOVERY_IVL;
+#elif ZMQ_VERSION_MAJOR == 2
+        int opt = ZMQ_RECOVERY_IVL_MSEC;
+#endif
+        int64v ivl = ivl_;
+
+        _M_setsockopt(opt, ivl);
+    }
+
+    void
+    socket::send_buffer(int size_)
+    {
+        uint64v size = size_;
+
+        _M_setsockopt(ZMQ_SNDBUF, size);
+    }
+
+    void
+    socket::recv_buffer(int size_)
+    {
+        uint64v size = size_;
+
+        _M_setsockopt(ZMQ_RCVBUF, size);
+    }
+
+    void
+    socket::linger(int linger_)
+    {
+        _M_setsockopt(ZMQ_LINGER, linger_);
+    }
+
+    void
+    socket::reconnect_ivl(int ivl_, int max_)
+    {
+        _M_setsockopt(ZMQ_RECONNECT_IVL, ivl_);
+        _M_setsockopt(ZMQ_RECONNECT_IVL_MAX, max_);
+    }
+
+    void
+    socket::backlog(int backlog_)
+    {
+        _M_setsockopt(ZMQ_BACKLOG, backlog_);
+    }
+
+    void
+    socket::max_msg_size(int64_t max_size_)
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        _M_setsockopt(ZMQ_MAXMSGSIZE, max_size_);
+#elif ZMQ_VERSION_MAJOR == 2
+        _M_max_msg_size = max_size_;
+#endif
+    }
+
+    void
+    socket::send_hwm(int hwm_)
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int hwm_type = ZMQ_SNDHWM;
+#elif ZMQ_VERSION_MAJOR == 2
+        int hwm_type = ZMQ_HWM;
+#endif
+        uint64v hwm = hwm_;
+
+        _M_setsockopt(hwm_type, hwm);
+    }
+
+    void
+    socket::recv_hwm(int hwm_)
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int hwm_type = ZMQ_RCVHWM;
+#elif ZMQ_VERSION_MAJOR == 2
+        int hwm_type = ZMQ_HWM;
+#endif
+        uint64v hwm = hwm_;
+
+        _M_setsockopt(hwm_type, hwm);
+    }
+
+    void
+    socket::multicast_hops(int hops_)
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        _M_setsockopt(ZMQ_MULTICAST_HOPS, hops_);
+#elif ZMQ_VERSION_MAJOR == 2
+        return;
+#endif
+    }
+
+    void
+    socket::send_timeout(int timeout_)
+    {
+        _M_sendtimeo = timeout_;
+
+#if ZMQ_VERSION_MAJOR == 3
+        _M_setsockopt(ZMQ_SNDTIMEO, _M_sendtimeo);
+#endif
+    }
+
+    void
+    socket::recv_timeout(int timeout_)
     {
         _M_recvtimeo = timeout_;
 
 #if ZMQ_VERSION_MAJOR == 3
-        setsockopt(ZMQ_RCVTIMEO, &_M_recvtimeo, sizeof(_M_recvtimeo));
+        _M_setsockopt(ZMQ_RCVTIMEO, _M_recvtimeo);
+#endif
+    }
+
+    void
+    socket::ipv4_only(bool ipv4_only_)
+    {
+#if ZMQ_VERSION_MAJOR == 3
+        int ipv4 = ipv4_only_;
+
+        _M_setsockopt(ZMQ_IPV4ONLY, ipv4);
+#elif ZMQ_VERSION_MAJOR == 2
+        return;
 #endif
     }
 
@@ -154,6 +514,15 @@ namespace cloudless
     socket::poll_item() throw()
     {
         return *_M_ip;
+    }
+
+    int
+    socket::_M_get_events() const
+    {
+        uint32v event;
+
+        _M_getsockopt(ZMQ_EVENTS, event);
+        return event;
     }
 
 } // namespace cloudless
--- cloudless/socket.hpp
+++ cloudless/socket.hpp
@@ -34,6 +34,27 @@
 namespace cloudless
 {
 
+namespace socket_type
+{
+
+    static const int PAIR = ZMQ_PAIR;
+    static const int PUB = ZMQ_PUB;
+    static const int SUB = ZMQ_SUB;
+    static const int REQ = ZMQ_REQ;
+    static const int REP = ZMQ_REP;
+    static const int XREQ = ZMQ_XREQ;
+    static const int XREP = ZMQ_XREP;
+    static const int PULL = ZMQ_PULL;
+    static const int PUSH = ZMQ_PUSH;
+    static const int XPUB = ZMQ_XPUB;
+    static const int XSUB = ZMQ_XSUB;
+
+    // Backwards compatibility
+    static const int DEALER = ZMQ_XREQ;
+    static const int ROUTER = ZMQ_XREP;
+
+} // namespace socket_type
+
     struct LIBCLOUDLESS_EXPORT socket : details::zsocket
     {
         socket(context& context_, int type_);
@@ -44,17 +65,58 @@ namespace cloudless
         void connect(const std::string& addr_);
         void connect(const address& addr_);
 
-        bool send(messages& msgs_, bool block_ = true);
-        bool recv(messages& msgs_, bool block_ = true);
+        virtual bool send(messages& msgs_, bool block_ = true);
+        virtual bool recv(messages& msgs_, bool block_ = true);
 
-        void set_send_timeout(int timeout_);
-        void set_recv_timeout(int timeout_);
+        uint64_t affinity() const;
+        std::string identity() const;
+        int rate() const;
+        int recovery_ivl() const;
+        int send_buffer() const;
+        int recv_buffer() const;
+        bool recv_more() const;
+        socket_t fd() const;
+        bool can_send() const;
+        bool can_recv() const;
+        int type() const;
+        int linger() const;
+        int reconnect_ivl() const;
+        int reconnect_ivl_max() const;
+        int backlog() const;
+        int64_t max_msg_size() const;
+        int send_hwm() const;
+        int recv_hwm() const;
+        int multicast_hops() const;
+        int send_timeout() const;
+        int recv_timeout() const;
+        bool ipv4_only() const;
+
+        void affinity(uint64_t affinity_);
+        void identity(const std::string& identity_);
+        void rate(int rate_);
+        void recovery_ivl(int ivl_);
+        void send_buffer(int size_);
+        void recv_buffer(int size_);
+        void linger(int linger_);
+        void reconnect_ivl(int ivl_, int max_ = 0);
+        void backlog(int backlog_);
+        void max_msg_size(int64_t max_size_);
+        void send_hwm(int hwm_);
+        void recv_hwm(int hwm_);
+        void multicast_hops(int hops_);
+        void send_timeout(int timeout_);
+        void recv_timeout(int timeout_);
+        void ipv4_only(bool ipv4_only_ = true);
 
         pollitem& poll_item() throw();
 
     private:
+        int _M_get_events() const;
+
+    private:
         int _M_recvtimeo;
         int _M_sendtimeo;
+        int64_t _M_max_msg_size;
         details::shared_ptr<pollitem> _M_ip;
     };
 
