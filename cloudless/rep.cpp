3750197833e89404b73ff1c14b8976d765818111
Amr Ali <amr@databracket.com>
2011-11-20 19:45:11 +0200

    Add REQ/REP pattern wrapper

---
+++ cloudless/rep.cpp
@@ -0,0 +1,32 @@
+/*
+    Copyright (c) 2011 Databracket, LLC.
+    Copyright (c) 2011 Other contributors as noted in the AUTHORS file
+
+    This file is part of Cloudless.
+
+    Cloudless is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Cloudless is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <cloudless/rep.hpp>
+
+namespace cloudless
+{
+
+    // rep
+
+    rep::rep(context& context_) :
+        socket(context_, socket_type::REP)
+    {}
+
+} // namespace cloudless
---
+++ cloudless/req.hpp
@@ -0,0 +1,38 @@
+/*
+    Copyright (c) 2011 Databracket, LLC.
+    Copyright (c) 2011 Other contributors as noted in the AUTHORS file
+
+    This file is part of Cloudless.
+
+    Cloudless is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Cloudless is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __CLOUDLESS_REQ_HPP
+#define __CLOUDLESS_REQ_HPP
+
+#include <cloudless/details/export.hpp>
+#include <cloudless/context.hpp>
+#include <cloudless/socket.hpp>
+
+namespace cloudless
+{
+
+    struct LIBCLOUDLESS_EXPORT req : socket
+    {
+        req(context& context_);
+    };
+
+} // namespace cloudless
+
+#endif // __CLOUDLESS_REQ_HPP
