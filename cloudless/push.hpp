cae621b1a4f3309ae67ae09a6c97a9aac2da0615
Amr Ali <amr@databracket.com>
2011-11-20 19:44:43 +0200

    Add PUSH/PULL pattern wrapper

---
+++ cloudless/push.hpp
@@ -0,0 +1,40 @@
+/*
+    Copyright (c) 2011 Databracket, LLC.
+    Copyright (c) 2011 Other contributors as noted in the AUTHORS file
+
+    This file is part of Cloudless.
+
+    Cloudless is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Cloudless is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __CLOUDLESS_PUSH_HPP
+#define __CLOUDLESS_PUSH_HPP
+
+#include <cloudless/details/export.hpp>
+#include <cloudless/context.hpp>
+#include <cloudless/socket.hpp>
+
+namespace cloudless
+{
+
+    struct LIBCLOUDLESS_EXPORT push : socket
+    {
+        push(context& context_);
+
+        virtual bool recv(message& msg_, bool block_ = true);
+    };
+
+} // namespace cloudless
+
+#endif // __CLOUDLESS_PUSH_HPP
