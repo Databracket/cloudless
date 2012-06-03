029944afac56ef715ad4a5ae35376bcda5c3e566
Amr Ali <amr@databracket.com>
2012-06-03 05:21:43 +0200

    Add event-driven device

---
+++ cloudless/device.cpp
@@ -0,0 +1,127 @@
+/**
+ * @file
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2012 Databracket LLC.
+ * Copyright (c) 2012 Other contributors as noted in the AUTHORS file
+ *
+ * This file is part of Cloudless.
+ *
+ * Cloudless is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cloudless is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ * An event-driven communication device.
+*/
+
+#include <cloudless/details/shared_ptr.hpp>
+#include <cloudless/device.hpp>
+#include <cloudless/pollitem.hpp>
+
+namespace cloudless
+{
+
+    // device
+
+    device::device(const edges& edges) :
+        details::thread(), _M_edges(edges)
+    {}
+
+    edges&
+    device::get_edges() const
+    {
+        return const_cast<edges&>(_M_edges);
+    }
+
+    void
+    device::prologue()
+    {
+        edges::iterator edges_it;
+
+        // Loop over all edges
+        for (edges_it = _M_edges.begin();
+                edges_it != _M_edges.end(); ++edges_it)
+        {
+            edgepoint::iterator point_it;
+
+            // Loop over all edge points
+            for (point_it = edges_it->second.begin();
+                    point_it != edges_it->second.end(); ++point_it)
+            {
+                // Check if a 0MQ context is specified, if not use a global one
+                if (!point_it->second.pcontext)
+                    point_it->second.pcontext =
+                        details::shared_ptr<context>(context::instance());
+
+                // Initialize a 0MQ socket for each point
+                point_it->second.psocket = details::shared_ptr<socket>(
+                        new socket(*point_it->second.pcontext,
+                            point_it->second.socket_type));
+
+                // Add pollitem for each socket to a poller
+                _M_poller.add(edges_it->first + point_it->first,
+                        point_it->second.psocket->poll_item()
+                            .register_event(poll_events::IN)
+                            );
+            }
+        }
+
+        on_init();
+    }
+
+    void
+    device::body()
+    {
+        if (_M_poller.poll(-1))
+        {
+            edges::iterator edges_it;
+            std::string pollitem_name = _M_poller.get_triggered();
+
+            // Loop over all edges
+            for (edges_it = _M_edges.begin();
+                    edges_it != _M_edges.end(); ++edges_it)
+            {
+                // Get the first part of a pollitem_name; edge ID
+                std::string edge_id(pollitem_name.c_str(), edges_it->first.size());
+
+                // Move early to the next edge if this one doesn't match
+                if (edge_id != edges_it->first)
+                    continue;
+
+                edgepoint::iterator point_it;
+
+                // Loop over all edge points
+                for (point_it = edges_it->second.begin();
+                        point_it != edges_it->second.end(); ++point_it)
+                {
+                    // Move to the next edge point if this one doesn't match
+                    if (pollitem_name != (edges_it->first + point_it->first))
+                        continue;
+
+                    // Pass edge's ID and point's ID to on_recv()
+                    on_recv(edges_it->first, point_it->first);
+                    return;
+                }
+            }
+        }
+    }
+
+    void
+    device::epilogue()
+    {
+        on_shutdown();
+    }
+
+} // namespace cloudless
---
+++ cloudless/device.hpp
@@ -0,0 +1,104 @@
+/**
+ * @file
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2012 Databracket LLC.
+ * Copyright (c) 2012 Other contributors as noted in the AUTHORS file
+ *
+ * This file is part of Cloudless.
+ *
+ * Cloudless is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cloudless is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ * An event-driven communication device.
+*/
+
+#ifndef CLOUDLESS_DEVICE_HPP
+#define CLOUDLESS_DEVICE_HPP
+
+#include <string>
+
+#include <cloudless/details/export.hpp>
+#include <cloudless/details/thread.hpp>
+#include <cloudless/poller.hpp>
+#include <cloudless/edges.hpp>
+
+namespace cloudless
+{
+
+    // device
+
+    class LIBCLOUDLESS_EXPORT device : public details::thread
+    {
+    public:
+        device(const edges& edges);
+        virtual ~device() {}
+
+    protected:
+
+        /**
+         * A virtual function to be optionally overrided by a child of this class.
+         * This function will be executed only once before starting to handle
+         * any events.
+         */
+        virtual void on_init()
+        {
+            // No default initialization function configured.
+            // Feel free to override this function to execute
+            // something once before entering on_recv().
+        }
+
+        /**
+         * A pure virtual function to be overrided by a child of this class.
+         * This function will be called on the event one of the edge points
+         * received a message.
+         *
+         * @param edge_id an edge ID.
+         * @param edgepoint_id a edgepoint ID.
+         */
+        virtual void on_recv(const std::string& edge_id, const std::string& edgepoint_id) = 0;
+
+        /**
+         * A virtual function to be optionally overrided by a child of this class.
+         * This function will be executed only once before the device thread terminates.
+         */
+        virtual void on_shutdown()
+        {
+            // No default shutdown function configured.
+            // Feel free to override this function to execute
+            // right before the destruction.
+        }
+
+        /**
+         * Get a reference to the edges container.
+         *
+         * @return a reference to the edges container.
+         */
+        edges& get_edges() const;
+
+    private:
+        void prologue();
+        void body();
+        void epilogue();
+
+    private:
+        edges _M_edges;
+        poller _M_poller;
+    };
+
+} // namespace cloudless
+
+#endif // CLOUDLESS_DEVICE_HPP
